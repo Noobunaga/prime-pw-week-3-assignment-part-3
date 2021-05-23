fc49e9e6edbbe7e1e8cdc1a4b00861ed47e60883
Pouapheng Yang <phewy06@yahoo.com>
2021-05-23 00:36:06 -0500

    finished

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -18,13 +18,15 @@ console.log('3. Item at index 2 is:');
 let i = supplyChanges[1];
 console.log(i);
 
-
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+console.log(supplyChanges.pop());
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push('25')
+console.log(supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -32,6 +34,18 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, skip it. Do not log it to the console.
 //    - if the value is negative, format the log as 'Part count -x.'
 console.log('6. Showing supplyChanges...');
+for( let x=0; x<supplyChanges.length; x++){
+  console.log( supplyChanges[ x ] );
+  if (supplyChanges [ x ] > 0){
+    console.log( 'Added x parts.');
+    }
+    if (supplyChanges [ x ] < 0){
+      console.log('Part count -x.')
+    }
+  }
+
+
+
 
 
 // STRETCH GOALS
